9765a925dc8305c3162c787ef0f90dbe9734179a
Albertomaria <albertomaria.moro@gmail.com>
2018-11-15 11:20:09 -0500

    add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='algebra',
+        version='1.0',
+        description='a small algebra manipulation package',
+        author='Albertomaria Moro',
+        author_email='al.mo@outlook.com',
+        url='',
+        )
